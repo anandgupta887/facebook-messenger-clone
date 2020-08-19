bd29eea17a9c52206a97cbe477ca7b32c575c3af
Anand <anandgupta887@yahoo.com>
2020-08-19 19:28:47 +0530

    adding input functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <input type='text'></input>
-      <button type='submit'>submit</button>
+      <h1>Hello User</h1>
+      <form>
+        <input onChange={(e) => setInput(e.target.value)} value={input} />
+        <button onClick={handleClick} type="submit">
+          submit
+        </button>
+      </form>
+      <ul>
+        {messages.map((message) => (
+          <li>{message}</li>
+        ))}
+      </ul>
     </div>
   );
 }
