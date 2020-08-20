07e3d60464221273527714c01590600f4dc71e5c
Anand <anandgupta887@yahoo.com>
2020-08-20 19:40:21 +0530

    adding animation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SendIcon from "@material-ui/icons/Send";
 import Message from "./Message";
 import db from "./firebase";
 import firebase from "firebase";
+import FlipMove from "react-flip-move";
 
 function App() {
   const [input, setInput] = useState("");
@@ -15,7 +16,12 @@ function App() {
     db.collection("messages")
       .orderBy("timestamp", "desc")
       .onSnapshot((snapshot) => {
-        setMessages(snapshot.docs.map((doc) => doc.data()));
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            message: doc.data(),
+          }))
+        );
       });
   }, []);
 
@@ -30,7 +36,7 @@ function App() {
       username: username,
       text: input,
     });
-    setMessages([...messages, { username: username, text: input }]);
+    // setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
@@ -58,10 +64,11 @@ function App() {
           </Button>
         </FormControl>
       </form>
-
-      {messages.map((message) => (
-        <Message username={username} message={message} />
-      ))}
+      <FlipMove>
+        {messages.map(({ id, message }) => (
+          <Message key={id} username={username} message={message} />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./Message.css";
 
-function Message({ message, username }) {
+const Message = forwardRef(({ message, username }, ref) => {
   const isUser = username === message.username;
   return (
-    <div className={`message ${isUser && "message__user"}`}>
+    <div className={`message ${isUser && "message__user"}`} ref={ref}>
       <Card className={`${isUser ? "message__sent" : "message__received"}`}>
         <CardContent>
           <Typography variant="h5" component="h5">
@@ -15,6 +15,6 @@ function Message({ message, username }) {
       </Card>
     </div>
   );
-}
+});
 
 export default Message;
