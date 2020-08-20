7087a350a3832b4c39c41ec8b1802da78707e73e
Anand <anandgupta887@yahoo.com>
2020-08-20 16:55:12 +0530

    adding message sending facility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
+import { Input, Button, FormControl, InputLabel } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
 
 function App() {
   const [input, setInput] = useState("");
@@ -15,16 +17,22 @@ function App() {
     <div className="App">
       <h1>Hello User</h1>
       <form>
-        <input onChange={(e) => setInput(e.target.value)} value={input} />
-        <button onClick={handleClick} type="submit">
-          submit
-        </button>
+        <FormControl>
+          <InputLabel htmlFor="my-input">Type your message here...</InputLabel>
+          <Input
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            id="my-input"
+          />
+        </FormControl>
+        <Button onClick={handleClick} disabled={!input} type="submit">
+          <SendIcon />
+        </Button>
       </form>
-      <ul>
-        {messages.map((message) => (
-          <li>{message}</li>
-        ))}
-      </ul>
+
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
