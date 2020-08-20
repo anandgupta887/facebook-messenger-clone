fb1ca820f122d6e6b70c333253f5133888b338ef
Anand <anandgupta887@yahoo.com>
2020-08-20 18:18:33 +0530

    getting username from prompt

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Input, Button, FormControl, InputLabel } from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
+import Message from "./Message";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    { username: "pratik", text: "Hello" },
+  ]);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    setUsername(prompt("enter your name:"));
+  }, []);
 
   const handleClick = (event) => {
     event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
   return (
     <div className="App">
-      <h1>Hello User</h1>
+      <h1>Hello {username}</h1>
       <form>
         <FormControl>
           <InputLabel htmlFor="my-input">Type your message here...</InputLabel>
@@ -24,14 +32,21 @@ function App() {
             value={input}
             id="my-input"
           />
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={handleClick}
+            disabled={!input}
+            type="submit"
+          >
+            Send Message
+            {/* <SendIcon /> */}
+          </Button>
         </FormControl>
-        <Button onClick={handleClick} disabled={!input} type="submit">
-          <SendIcon />
-        </Button>
       </form>
 
       {messages.map((message) => (
-        <p>{message}</p>
+        <Message username={username} message={message} />
       ))}
     </div>
   );
---
+++ src/Message.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./Message.css";
+
+function Message({ message, username }) {
+  const isUser = username === message.username;
+  return (
+    <div className={`message ${isUser && "message__user"}`}>
+      <Card className={`${isUser ? "message__sent" : "message__received"}`}>
+        <CardContent>
+          <Typography color="white" variant="h5" component="h5">
+            {message.username} : {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default Message;
